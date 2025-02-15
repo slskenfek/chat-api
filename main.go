c1b3ba3a1fbd7408c1a8f45120672eb694c467f7
DESKTOP-D2PFH23\yongjun <rladydwns248@gmail.com>
2025-02-15 22:38:22 +0900

    html 및 설정 분리

--- main.go
+++ main.go
@@ -3,24 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
-
-var upgrades = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func handelConnection(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrades.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("WebSocket 업그레이드 실패", err)
-	}
-	defer conn.Close()
-
-}
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
